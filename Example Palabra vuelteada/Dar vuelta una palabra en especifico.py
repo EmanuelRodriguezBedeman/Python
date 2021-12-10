3f08fc91dd9b0678852930db2d79161844cd1016
EmanuelRodriguezBedeman <93904438+EmanuelRodriguezBedeman@users.noreply.github.com>
2021-12-10 15:04:27 -0300

    Update and rename Dar vuelta una palabra en especifico.py to Vueltear una palabra

--- Example Palabra vuelteada/Dar vuelta una palabra en especifico.py
+++
@@ -1,8 +0,0 @@
-oracion = "Hola soy gardel me gusta el tinto con maní"
-palabras = oracion.split()
-
-for palabra in palabras:
-    if len(palabra) >= 5:
-        indice_palabra = palabras.index(palabra)
-        palabras[indice_palabra] = palabra[len(palabra)::-1]
-        print(" ".join(palabras))
